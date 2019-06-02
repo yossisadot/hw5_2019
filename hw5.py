dee07d4a66e672f8cc2ce90288aebb1506813318
yossi sadot <yossisadot@gmail.com>
2019-06-02 23:04:31 +0300

    bonus

--- hw5.py
+++ hw5.py
@@ -29,12 +29,12 @@ class QuestionnaireAnalysis:
         The first item being the number of people in a given bin.
         The second item being the bin edges.
         """
-        age_dist = plt.hist(self.data["age"], bins=np.linspace(0, 100, 11)) # Unlike in the instructions, the last bin is close-ended
+        # Unlike in the instructions, the last bin is close-ended
+        age_dist = plt.hist(self.data["age"], bins=np.linspace(0, 100, 11))
         # Plots the age distribution of the participants
         plt.xlabel('Age')
         plt.ylabel('Number of people')
-        plt.show(block=False)
-        # block is set to False so it won't interrupt with tests
+        plt.show()
         return age_dist
 
     def remove_rows_without_mail(self) -> pd.DataFrame:
@@ -44,7 +44,8 @@ class QuestionnaireAnalysis:
         the erroneous rows removed and the (ordinal) index after a reset.
         """
         # Creates a corrected copy of the df, because the next func uses the original df
-        with_valid_mail = self.data[self.data["email"].str.match(r'\w+[.\w+]*@\w+.\w+')].reset_index()
+        with_valid_mail = self.data[self.data["email"].str.match(
+            r'\w+[.\w+]*@\w+.\w+')].reset_index()
         return with_valid_mail
         
     def fill_na_with_mean(self) -> Union[pd.DataFrame, np.ndarray]:
@@ -57,12 +58,15 @@ class QuestionnaireAnalysis:
         """
         # Finds the subjects that didn't answer all questions
         nan_array = np.array(self.data.index[self.data.loc[:,'q1':'q5'].isna().any(1)])
-        # Regarding the next 2 lines:
-            # In line 65 I couldn't find a way to fill by row with one statement that
-            # covers all, or a range of, collumns at once without specifying each one.
-            # Since I specify each, I used 'mean_grade' (line 64) for readability
+        #########################################################################################
+        # Regarding the following 2 lines:                                                      #
+        # In line 68 I couldn't find a way to fill by row with one statement that covers all,   #
+        # or a range of, collumns at once without specifying each one (except by transpose, but #
+        # I don't like it). Since I specify each, I used 'mean_grade' (line 67) for readability #
+        #########################################################################################
         mean_grade = self.data.loc[:,'q1':'q5'].mean(axis=1)
-        self.data.fillna({'q1':mean_grade, 'q2':mean_grade, 'q3':mean_grade, 'q4':mean_grade, 'q5':mean_grade}, inplace=True)
+        self.data.fillna({'q1':mean_grade, 'q2':mean_grade, 'q3':mean_grade, 'q4':mean_grade, 
+            'q5':mean_grade}, inplace=True)
         return self.data, nan_array
 
 
@@ -74,7 +78,16 @@ class QuestionnaireAnalysis:
         the subject is above 40 years of age, and the average score in each of
         the five questions.
         """
-        #...
+        gender_age=self.data.groupby(['gender', self.data.age > 40])[[
+            'q1','q2','q3','q4','q5']].mean()
+        ########################################################################################
+        # The return data is different from the 'q4_corr.csv' and the 'avg_per_group.png' even #
+        # though the test passes successfully (when I print(test.correlate_gender_age()) I get #
+        # results different the csv). But I checked the real grouped data and it seems that    #
+        # the data from this function is accurate.                                             #
+        ########################################################################################
+        return gender_age
+        
 
 if __name__ == '__main__':
     w = 1
@@ -89,4 +102,4 @@ if __name__ == '__main__':
     test.show_age_distrib()
     test.remove_rows_without_mail()
     test.fill_na_with_mean()
-    
+    print(test.correlate_gender_age())
